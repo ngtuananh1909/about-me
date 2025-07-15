2ebcd4906acd3455e75c678cd43839c3533915cd
Tuananh <nta1909k7@gmail.com>
2025-07-15 09:51:04 +0700

    done

---
+++ js/script-game.js
@@ -0,0 +1,12 @@
+
+function showGame(imageName) {
+  const modal = document.getElementById("game-modal");
+  const img = document.getElementById("game-image");
+  img.src = "/picture/" + imageName;
+  modal.style.display = "flex";
+}
+
+function hideModal() {
+  document.getElementById("game-modal").style.display = "none";
+}
+
